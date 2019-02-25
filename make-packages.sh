7d3afe7600d80b3be0aba9ce14f1e04e3302f5fc
ChengChung Tseng <cctseng6048@gmail.com>
2019-02-25 13:36:44 +0800

    test

--- make-packages.sh
+++ make-packages.sh
@@ -2,6 +2,5 @@ rm Packages
 rm Packages.gz
 rm Packages.bz2
 ./dpkg-scanpackages.pl -m ./debs /dev/null > Packages
-bzip2 -fks Packages
-rem gzip -c9 Packages > Packages.gz
-rem bzip2 -c9 Packages > Packages.bz2
+gzip -c9 Packages > Packages.gz
+bzip2 -c9 Packages > Packages.bz2
