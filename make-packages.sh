db0155b9ccc99c9c108af8aecce2b9c81338d75c
ChengChung Tseng <cctseng6048@gmail.com>
2019-03-28 14:09:17 +0800

    0.0.12
    增加轉珠時間

--- make-packages.sh
+++ make-packages.sh
@@ -1,6 +1,4 @@
 rm Packages
-rm Packages.gz
 rm Packages.bz2
-./dpkg-scanpackages.pl -m ./debs /dev/null > Packages
-gzip -c9 Packages > Packages.gz
-bzip2 -c9 Packages > Packages.bz2
+dpkg-scanpackages -m debs > Packages
+bzip2 -fks Packages
